7c788b636bc1d417fe3323b2ca49c11f2c47c863
Derek Dyer <derek@digitalderek.com>
2015-10-02 23:27:54 -0400

    fixed bg image for production... I think

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,9 @@ Rails.application.configure do
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
+config.serve_static_assets = true
+config.assets.compile = true
+
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
